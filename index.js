8f989966490f3749f1bd41ed5e2e36a8bc1939b1
Haider <213haiderali@gmail.com>
2023-06-23 21:40:53 +0530

    Add validations for put and post routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const routes = require("express").Router();
 const { tasks: taskData } = require("./tasks");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
+const { body, validationResult } = require("express-validator");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,23 +25,41 @@ routes.get("/tasks/:id", (req, res) => {
   res.json({ params: taskData.tasks.find((item) => item.id == req.params.id) });
 });
 
-routes.post("/tasks", (req, res) => {
+const validations = [
+  body("title").notEmpty(),
+  body("description").notEmpty(),
+  body("completitionStatus").isBoolean(),
+];
+
+routes.post("/tasks", ...validations, (req, res) => {
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    return res.json({ errors: result.array() });
+  }
   const { id, title, description } = req.body;
   const newTask = {
     id,
     title,
     description,
+    completitionStatus,
   };
   taskData.push(newTask);
   res.json(taskData);
 });
 
-routes.put("/tasks/:id", (req, res) => {
-  const { id, title, description } = req.body;
+routes.put("/tasks/:id", ...validations, (req, res) => {
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    return res.json({ errors: result.array() });
+  }
+  const { id, title, description, completitionStatus } = req.body;
   const taskToUpdate = {
     id,
     title,
     description,
+    completitionStatus,
   };
   const index = taskData.findIndex((task) => task.id === id);
   if (index === -1) {
--- tasks.js
+++ tasks.js
@@ -1,7 +1,22 @@
 const tasks = [
-  { id: 1, title: "Task 1", description: "Rise early in the morning" },
-  { id: 2, title: "Task 2", description: "Take a bath" },
-  { id: 3, title: "Task 3", description: "Eat breakfast" },
+  {
+    id: 1,
+    title: "Task 1",
+    description: "Rise early in the morning",
+    completitionStatus: true,
+  },
+  {
+    id: 2,
+    title: "Task 2",
+    description: "Take a bath",
+    completitionStatus: false,
+  },
+  {
+    id: 3,
+    title: "Task 3",
+    description: "Eat breakfast",
+    completitionStatus: true,
+  },
 ];
 
 const getTasks = () => tasks;
